ee7cdba71f264150b6720719ad8b7b079b08fdbb
Chad Cooper <spazmochad@gmail.com>
2018-01-26 14:02:33 +0000

    Added Boundary model

---
+++ src/Game.CodingExercise.Game/Models/Boundary.cs
@@ -0,0 +1,14 @@
+﻿namespace Game.CodingExercise.Game.Models
+{
+    public class Boundary
+    {
+        public int Min { get; }
+        public int Max { get; }
+
+        public Boundary(int max, int min)
+        {
+            Max = max;
+            Min = min;
+        }
+    }
+}
