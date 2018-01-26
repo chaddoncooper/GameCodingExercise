d4953dbce808d2b0dabdae3dfc437d5f451178b1
Chad Cooper <spazmochad@gmail.com>
2018-01-26 14:06:22 +0000

    Added IPlayer interface

---
+++ src/Game.CodingExercise.Game/IPlayer.cs
@@ -0,0 +1,13 @@
+﻿namespace Game.CodingExercise.Game
+{
+    public interface IPlayer
+    {
+        bool MoveForward();
+        bool MoveBackward();
+        void TurnLeft();
+        void TurnRight();
+        void MoveUp();
+        void MoveDown();
+        string GetPosition();
+    }
+}
