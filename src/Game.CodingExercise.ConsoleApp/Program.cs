47f4bff3334983d3506c4b04e53453e4821a1f9c
Chad Cooper <spazmochad@gmail.com>
2018-01-26 17:51:07 +0000

    Added Console App

--- src/Game.CodingExercise.ConsoleApp/Program.cs
+++ src/Game.CodingExercise.ConsoleApp/Program.cs
@@ -4,10 +4,20 @@ namespace Game.CodingExercise.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var game = new Game();
+            var game = Game.GetDefaultSinglePlayerGame();
+            var inputString = "";
 
+            do
+            {
+                Console.Clear();
+                Console.WriteLine($"Current Position\n{game.GetPlayerPosition()}");
+                Console.WriteLine("Enter you move(s), exit to quit:");
+                inputString = Console.ReadLine();
+                game.Move(inputString);
+
+            } while (inputString != "exit");
         }
     }
 }
