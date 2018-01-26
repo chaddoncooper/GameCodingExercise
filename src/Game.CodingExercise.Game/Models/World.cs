bbdcd0d0e648af9b65c5303123f6e2960c944201
Chad Cooper <spazmochad@gmail.com>
2018-01-26 14:04:20 +0000

    Added World model

---
+++ src/Game.CodingExercise.Game/Models/World.cs
@@ -0,0 +1,16 @@
+﻿namespace Game.CodingExercise.Game.Models
+{
+    public class World
+    {
+        public readonly Boundary X;
+        public readonly Boundary Y;
+        public readonly Boundary Z;
+
+        public World(Boundary x, Boundary y, Boundary z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+}
